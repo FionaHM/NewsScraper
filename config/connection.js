332385d96ba9d1814cad7696ad57f5f683c73eab
FH <FH@Fionas-MacBook-Air.local>
2017-02-24 15:26:26 -0600

    done

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 // connect to the database
-var connection = mongoose.connect('mongodb://localhost:27017/newsscraperdb');
+var connection = mongoose.connect('MONGODB_URI');
 // export the connection
 module.exports = connection;
