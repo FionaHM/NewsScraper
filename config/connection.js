b6a09235731852ce99882d9dd644b3f06bf09fa0
FH <FH@Fionas-MacBook-Air.local>
2017-02-24 17:00:11 -0600

    added README

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 // connect to the database
-var connectionString = process.env.MONGODB_URI | 'mongodb://localhost:27017/newsscraperdb';
+// var connectionString = process.env.MONGODB_URI | 'mongodb://localhost:27017/newsscraperdb';
 // var connection = mongoose.connect(process.env.MONGODB_URI);
-var connection = mongoose.connect(connectionString);
+var connection = mongoose.connect(process.env.MONGODB_URI);
 // export the connection
 module.exports = connection;
